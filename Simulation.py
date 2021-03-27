88b56b6f134e7c9f06ea16b1c9f6c056582efd69
SuckMyLeg <juanito-146@hotmail.es>
2021-03-27 11:06:59 +0100

    Doing it again

--- Simulation.py
+++
@@ -1,9 +0,0 @@
-from main import Controller
-
-controller = Controller()
-
-
-controller.main()
-
-
-input()
---
+++ main/tests.py
@@ -0,0 +1,7 @@
+from phisics import *
+
+
+p = Phisic()
+
+
+input(p.get_rect_ect_from_2_points([1, 3], [2, -5]))
